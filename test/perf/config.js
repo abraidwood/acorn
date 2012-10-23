0bceac7a0d7d7e42f8618db3be232b99ea4f0706
Alistair Braidwood <alistair.braidwood@rulefinancial.com>
2012-10-23 18:49:30 +0100

    change sources and accuracy for perf tests

--- test/perf/config.js
+++ test/perf/config.js
@@ -1,13 +1,21 @@
 module.exports = {
 	acorn: require('../../acorn.js'),
 
-	maxTime: 1,
+	maxTime: 5,
 
 	sourcesDir: './3rdParty',
 
 	sources: [
-		{name: 'underscore-1.4.1'},
-		{name: 'backbone-0.9.2'}
+		{name: 'angular-1.0.2'},
+		{name: 'backbone-0.9.2'},
+		{name: 'codemirror-2.34'},
+//		{name: 'esprima'},
+		{name: 'jquery-1.8.2'},
+		{name: 'jquery.mobile-1.2.0'},
+//		{name: 'mootools-1.4.1'},
+//		{name: 'parse-js'},
+		{name: 'threejs-r51'},
+		{name: 'underscore-1.4.1'}
 	],
 
 	resultsFile: './results.csv'
