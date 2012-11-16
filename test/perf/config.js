524cd24daadf5c1ba0417bfe7a11cd36309ff77d
Alistair Braidwood <alistair.braidwood@rulefinancial.com>
2012-11-16 08:19:18 +0000

    node perf test

---
+++ test/perf/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	acorn: require('../../acorn.js'),
+
+	acornOptions: {
+		ecmaVersion: 5,
+		strictSemicolons: false,
+		allowTrailingCommas: true,
+		forbidReserved: false,
+		trackComments: false,
+		locations: false,
+		ranges: false,
+		program: null,
+		sourceFile: null
+	},
+
+	// Test Options
+	maxTime: 5,
+
+	sourcesDir: './3rdParty',
+
+	sources: [
+//		{name: 'angular-1.0.2'},
+		{name: 'backbone-0.9.2'},
+//		{name: 'codemirror-2.34'},
+//		{name: 'esprima'},
+//		{name: 'jquery-1.8.2'},
+//		{name: 'jquery.mobile-1.2.0'},
+//		{name: 'mootools-1.4.1'},
+//		{name: 'parse-js'},
+//		{name: 'threejs-r51'},
+		{name: 'underscore-1.4.1'}
+	],
+
+	resultsFile: './results.csv'
+};
